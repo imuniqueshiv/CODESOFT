c25de180b5257c084d6be981f328e5f7ed7d004c
Shiv <singhshivraj89969@gmail.com>
2025-12-02 16:03:17 +0530

    Fix cookie security and protected routes

--- LEVEL-3-TASK-2/client/src/AppRoutes.jsx
+++ LEVEL-3-TASK-2/client/src/AppRoutes.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import ProjectPage from './pages/ProjectPage';
+import Login from './pages/Login';
+import EmailVerify from './pages/EmailVerify';
+import ResetPassword from './pages/ResetPassword';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
 
 export default function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/project/:id' element={<ProjectPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Public Routes */}
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/email-verify' element={<EmailVerify />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+
+      {/* Protected Routes (Wrapped) */}
+      <Route 
+        path='/dashboard' 
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } 
+      />
+      
+      <Route 
+        path='/project/:id' 
+        element={
+          <ProtectedRoute>
+            <ProjectPage />
+          </ProtectedRoute>
+        } 
+      />
+    </Routes>
   );
 }
